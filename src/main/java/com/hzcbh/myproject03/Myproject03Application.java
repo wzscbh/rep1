b46257c39e8e5c6e3f771255a9fbb17bcbd51944
wzscbh <wzbh@21cn.com>
2019-10-06 21:44:14 +0800

    测试1

---
+++ src/main/java/com/hzcbh/myproject03/Myproject03Application.java
@@ -0,0 +1,13 @@
+package com.hzcbh.myproject03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myproject03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Myproject03Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hzcbh/myproject03/Myproject03ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hzcbh.myproject03;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Myproject03ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
